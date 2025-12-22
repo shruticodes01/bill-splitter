c70b0ddac2d657ae2bad3e7d700c306c6700e996
Shruti <dev.shruti27@gmail.com>
2025-12-22 17:20:56 +0100

    fix container size

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -12,7 +12,7 @@ export default function Container({
 }: ContainerProps) {
   return (
     <div
-      className={`w-full h-screen max-md:max-w-md max-md:max-h-233.25 md:max-h-256 md:max-w-152 lg:max-w-227 mx-auto ${className}`}
+      className={`w-full h-screen max-md:max-w-md max-md:max-h-233.25 md:max-h-256 md:max-w-152 lg:max-w-230 mx-auto ${className}`}
       {...props}
     >
       {children}
